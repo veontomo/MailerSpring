b85d5488dc3fba0194b882c4bd5a28751cfd057e
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-22 19:07:50 +0200

    Split the build.gradle script btw top-level and Reception-level ones

--- Reception/src/main/kotlin/com/veontomo/Reception/ReceptionApplication.kt
+++ Reception/src/main/kotlin/com/veontomo/Reception/ReceptionApplication.kt
@@ -1,5 +1,8 @@
 package com.veontomo.Reception
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
 @SpringBootApplication
 class ReceptionApplication
 
