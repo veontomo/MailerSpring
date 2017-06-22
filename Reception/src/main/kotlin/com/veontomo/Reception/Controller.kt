f19d81dc8d20aadcc81513ed671f77de7749e440
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-22 20:16:38 +0200

    Add dumb controllers

---
+++ Reception/src/main/kotlin/com/veontomo/Reception/Controller.kt
@@ -0,0 +1,22 @@
+package com.veontomo.Reception
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.stereotype.Controller
+
+/**
+ * Reception controller
+ *
+ */
+@Controller
+@EnableAutoConfiguration
+class Controller() {
+
+    @RequestMapping("/main")
+    fun mainpage(@RequestParam(value = "name", required = false, defaultValue = "User") name: String, model: Model): String {
+        model.addAttribute("name", name)
+        return "mainpage"
+    }
+}
