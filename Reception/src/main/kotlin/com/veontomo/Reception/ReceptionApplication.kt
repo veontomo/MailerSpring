166ddcb505a31c2c8fb2848cad645782799a3c82
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-22 16:58:54 +0200

    Delete and create the reception module again

--- Reception/src/main/kotlin/com/veontomo/Reception/ReceptionApplication.kt
+++ Reception/src/main/kotlin/com/veontomo/Reception/ReceptionApplication.kt
@@ -1,15 +1,9 @@
 package com.veontomo.Reception
 
-/**
- * Reception application entry point.
- */
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
 @SpringBootApplication
 class ReceptionApplication
 
+
 fun main(args: Array<String>) {
-//    System.setProperty("server.port", "8070");
     SpringApplication.run(ReceptionApplication::class.java, *args)
 }
