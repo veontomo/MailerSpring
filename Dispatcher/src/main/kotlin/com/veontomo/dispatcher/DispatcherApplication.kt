6018145f6d11aae3c041fcf2bb921c5dc7ba6e80
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-27 09:04:50 +0200

    Add a link to where the code is taken

--- Dispatcher/src/main/kotlin/com/veontomo/dispatcher/DispatcherApplication.kt
+++ Dispatcher/src/main/kotlin/com/veontomo/dispatcher/DispatcherApplication.kt
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class DispatcherApplication{
+class DispatcherApplication {
+    // The code below is taken from here:
+    // https://spring.io/blog/2016/02/15/developing-spring-boot-applications-with-kotlin
     @Bean
     open fun init(repository: CustomerRepository) = CommandLineRunner {
         println("Saving customers")
